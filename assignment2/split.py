65016cc30b0664bf88bc7cdbd6198d46bd2b4ab1
invelop <webfenglin@163.com>
2021-12-01 16:00:30 +0800

    correct assignment2

--- assignment2/split.py
+++ assignment2/split.py
@@ -5,9 +5,10 @@
 """
 
 filename = "test"
-with open(filename, mode='r+') as f:
+content = ""
+with open(filename, mode='r') as f:
     content = f.read().strip().split(' ')
-    f.seek(0)
-    f.truncate()
-    content = '\n'.join([' '.join(content[i:i+5]) for i in range(0, 20, 5)])
+
+content = '\n'.join([' '.join(content[i:i+5]) for i in range(0, 20, 5)])
+with open("new", mode="w") as f:
     f.write(content)
